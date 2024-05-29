2a8e60f13da9faee86ff8e65a92448ec5cabfc87
avocadro <jordan.mastellone@gmail.com>
2024-05-30 00:18:55 +1000

    feat: added etl and viz roles for snowflake, drafted ETL app, added template deployment yaml

--- ETL/app.py
+++ ETL/app.py
@@ -1,62 +1,65 @@
 import requests
-import pandas as pd
 import snowflake.connector
-import json
 import os
 
 # Configuration
-API_URL = "https://api.example.com/data"  # Replace with the actual API URL
-SNOWFLAKE_USER = os.getenv('SNOWFLAKE_USER')
-SNOWFLAKE_PASSWORD = os.getenv('SNOWFLAKE_PASSWORD')
-SNOWFLAKE_ACCOUNT = os.getenv('SNOWFLAKE_ACCOUNT')
-SNOWFLAKE_WAREHOUSE = os.getenv('SNOWFLAKE_WAREHOUSE')
-SNOWFLAKE_DATABASE = os.getenv('SNOWFLAKE_DATABASE')
-SNOWFLAKE_SCHEMA = os.getenv('SNOWFLAKE_SCHEMA')
-SNOWFLAKE_TABLE = os.getenv('SNOWFLAKE_TABLE')
 
-def fetch_secrets():
-    if os.getenv('ENVIRONMENT') == 'local':
-        SNOWFLAKE_PASSWORD = os.getenv("LOCALDEV_SNOWFLAKE_PASSWORD")
-        API_KEY = os.getenv("LOCALDEV_ALPHAVANTAGE_API_KEY")
-    elif os.getenv('ENVIRONMENT') == 'AKSPRD':
-        SNOWFLAKE_PASSWORD = os.getenv("AKSPRD_SNOWFLAKE_PASSWORD")
-        API_KEY = os.getenv("AKSPRD_ALPHAVANTAGE_API_KEY")
+ENVIRONMENT = os.getenv('ENVIRONMENT')
+if ENVIRONMENT not in ['LOCAL_DEV', 'AKS_PRD']:
+    raise ValueError(f"Environment '{ENVIRONMENT}' is not valid. Expected 'LOCAL_DEV' or 'AKS_PRD'.")
+else:
+    SNOWFLAKE_USER = 'APP_PYTHON_AKS'
+    SNOWFLAKE_PASSWORD = os.getenv('SNOWFLAKE_PASSWORD')
+    SNOWFLAKE_ACCOUNT = 'AQMTEBD.JX70701'
+    SNOWFLAKE_WAREHOUSE = 'COMPUTE_WH'
+    ALPHAVANTAGE_API_KEY = os.getenv('ALPHAVANTAGE_API_KEY')
 
-def fetch_data(api_url):
-    response = requests.get(api_url)
-    response.raise_for_status()
-    return response.json()
-
-def load_data_to_snowflake(data):
-    # Connect to Snowflake
-    conn = snowflake.connector.connect(
-        user=SNOWFLAKE_USER,
-        password=SNOWFLAKE_PASSWORD,
-        account=SNOWFLAKE_ACCOUNT,
-        warehouse=SNOWFLAKE_WAREHOUSE,
-        database=SNOWFLAKE_DATABASE,
-        schema=SNOWFLAKE_SCHEMA
+conn = snowflake.connector.connect(
+    user=SNOWFLAKE_USER,
+    password=SNOWFLAKE_PASSWORD,
+    account=SNOWFLAKE_ACCOUNT,
+    warehouse=SNOWFLAKE_WAREHOUSE
     )
 
-    # Convert data to DataFrame
-    df = pd.DataFrame(data)
+def get_and_put_data(conn):
+    cursor = conn.cursor()
+    select_query =  ''' 
+        with ticker_latest as (
+            select 
+                ticker_symbol,
+                max_by(company_name, insert_datetime_ntz) as company_name,
+                max_by(stock_exchange, insert_datetime_ntz) as stock_exchange,
+                max_by(is_active, insert_datetime_ntz) as is_active_latest
+            from polar_fleece_apac_prd.finance_ods.av_tickers_sc_dim
+            group by ticker_symbol
+            )
+            select ticker_symbol
+            from ticker_latest 
+            where is_active_latest = true 
+    '''
+    cursor.execute(select_query)
+    results = cursor.fetchall()
+    cursor.close()
+    for result in results:
+        # tickers are just in the first column (0)
+        ticker_symbol = result[0]
+        api_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker_symbol}&outputsize=compact&apikey={ALPHAVANTAGE_API_KEY}&datatype=json" 
+        # Make an API call for each value
+        response = requests.get(api_url)
+        api_data = response.json()
+        # Insert each row of the API call result JSON into the Snowflake table
+        insert_data(conn, api_data)
 
-    # Write data to Snowflake
-    success, nchunks, nrows, _ = df.to_sql(
-        SNOWFLAKE_TABLE,
-        conn,
-        if_exists='append',  # Change to 'replace' if you want to overwrite the table
-        index=False
-    )
-
-    print(f"Inserted {nrows} rows into {SNOWFLAKE_TABLE}")
-
-    # Close the connection
-    conn.close()
+def insert_data(conn, data):
+    insert_query = f"insert into polar_fleece_apac_prd.finance_ods.av_daily_fact VALUES (?, ?)"  # Example query - needs to be adjusted
+    cursor = conn.cursor()
+    for row in data:
+        cursor.execute(insert_query, row)  # Assuming row is a tuple of values matching the table schema
+    cursor.close()
+    conn.commit()
 
 def main():
-    data = fetch_data(API_URL)
-    load_data_to_snowflake(data)
+    get_and_put_data(conn)
 
 if __name__ == "__main__":
     main()
--- polar-fleece-apac-prd/account-admin.sql
+++ polar-fleece-apac-prd/account-admin.sql
@@ -1,11 +1,7 @@
--- Some administrative commands for setting up core components of the Polar Fleece database
+-- Database and schema creation statements plus access management
 
--- Create a domain owner role, which is restricted to ownership privileges of finance domain objects
-create role if not exists ROLE_FINANCE_OWNER_PRD;
-grant usage on database polar_fleece_apac_prd to role ROLE_FINANCE_OWNER_PRD;
-grant usage on warehouse compute_wh to role ROLE_FINANCE_OWNER_PRD;
-
--- Create split Finance schema, to separate the data from the logic as per the data mesh philosophy
+-- Create split Finance schema
+---- separate the data from the logic as per the data mesh philosophy
 create schema polar_fleece_apac_prd.finance_ods 
     with managed access
     comment = 'Operational data store layer for finance data that is ingested into the Polar Fleece database. This serves as a source of transactional data prior to analysis.';
@@ -13,11 +9,36 @@ create schema polar_fleece_apac_prd.finance_analytics
     with managed access
     comment = 'Analytics layer for finance data that has been transformed from finance_ODS. This layer is for developing views for users and reporting tools.';
 
--- Grant the Finance owner role full access privileges over the Finance domain
+-- Create a domain owner role and grant it to a human user
+---- restricted to ownership privileges of finance domain objects
+create role if not exists role_finance_owner_prd;
+grant usage on database polar_fleece_apac_prd to role role_finance_owner_prd;
+grant usage on warehouse compute_wh to role role_finance_owner_prd;
+---- Grant the Finance owner role full access privileges over the Finance domain
 grant ownership on schema polar_fleece_apac_prd.finance_ods to role role_finance_owner_prd revoke current grants;
 grant ownership on schema polar_fleece_apac_prd.finance_analytics to role role_finance_owner_prd revoke current grants;
-
--- Create a user and grant them finance domain ownership
+---- Create a user and grant them finance domain ownership
 create user user_jordan_prd;
-alter user user_jordan_prd set default_warehouse = compute_wh;
+alter user user_jordan_prd set default_warehouse = compute_wh; --password set in web ui
 grant role role_finance_owner_prd to user user_jordan_prd;
+
+-- Create ETL and Viz roles and grant them to a bot user
+---- Create a role for handling the ETL of the alphavantage API
+---- The role will need to be able to select from finance-ODS objects
+---- The role will need to be able to insert into finance-ODS objects
+create role if not exists role_finance_etl_prd;
+grant usage on database polar_fleece_apac_prd to role role_finance_etl_prd;
+grant usage on warehouse compute_wh to role role_finance_etl_prd;
+grant select,insert on all tables in schema polar_fleece_apac_prd.finance_ods to role role_finance_etl_prd;
+---- Create a role for visualising Snowflake data
+---- The role will need to be able to select from finance-ODS and finance-analytics objects
+create role if not exists role_finance_viz_prd;
+grant usage on database polar_fleece_apac_prd to role role_finance_viz_prd;
+grant usage on warehouse compute_wh to role role_finance_viz_prd;
+grant select on all tables in database polar_fleece_apac_prd to role role_finance_viz_prd;
+grant select on all views in database polar_fleece_apac_prd to role role_finance_viz_prd;
+---- Create the bot user
+create user app_python_aks;
+alter user app_python_aks set default_warehouse = compute_wh; --password set in web ui
+grant role role_finance_viz_prd to user app_python_aks;
+grant role role_finance_etl_prd to user app_python_aks;
